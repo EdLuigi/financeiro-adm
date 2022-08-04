793ca0f8824caa0a2a983ca035c57bf4f8e09885
Luigi <luigi.ciuffi@hotmail.com>
2022-08-03 21:03:25 -0300

    login com template

--- src/routes/SignIn.js
+++ src/routes/SignIn.js
@@ -1,10 +1,8 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
+import LoadingButton from "@mui/lab/LoadingButton";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -12,35 +10,91 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Alert, Collapse, IconButton, InputAdornment } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../firebase/authContext";
 
-function Copyright(props) {
-    return (
-        <Typography
-            variant="body2"
-            color="text.secondary"
-            align="center"
-            {...props}
-        >
-            {"Copyright © "}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
-}
+// function Copyright(props) {
+//     return (
+//         <Typography
+//             variant="body2"
+//             color="text.secondary"
+//             align="center"
+//             {...props}
+//         >
+//             {"Copyright © "}
+//             <Link color="inherit" href="https://mui.com/">
+//                 Your Website
+//             </Link>{" "}
+//             {new Date().getFullYear()}
+//             {"."}
+//         </Typography>
+//     );
+// }
 
 const theme = createTheme();
 
 export default function SignIn() {
-    const handleSubmit = (event) => {
+    const [errorEmail, setErrorEmail] = useState("");
+    const [errorSenha, setErrorSenha] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const emailRegex =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    const [loading, setLoading] = useState(false);
+    const [erro, setErro] = useState("");
+    const navigate = useNavigate();
+    const { login } = useAuth();
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get("email"),
-            password: data.get("password"),
-        });
+    };
+
+    const verifyEmail = () => {
+        if (email == "") {
+            setErrorEmail("Insira um email válido");
+            return 1;
+        }
+        if (!emailRegex.test(email)) {
+            setErrorEmail("O email inserido não está formatado corretamente");
+            return 1;
+        }
+        return 0;
+    };
+
+    const verifyPassword = () => {
+        if (password == "") {
+            setErrorSenha("Insira sua senha");
+            return 1;
+        }
+        return 0;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setErro("");
+        setErrorEmail("");
+        setErrorSenha("");
+
+        if (+verifyEmail() + +verifyPassword() != 0) return;
+
+        try {
+            setLoading(true);
+
+            await login(email, password);
+
+            navigate("/");
+        } catch (e) {
+            console.log("erro: " + e);
+            setErro("Não foi possível fazer login, email ou senha incorretos.");
+        }
+        setLoading(false);
     };
 
     return (
@@ -67,40 +121,63 @@ export default function SignIn() {
                         noValidate
                         sx={{ mt: 1 }}
                     >
+                        <Collapse in={erro !== ""}>
+                            <Alert severity="error" sx={{ mb: 1 }}>
+                                {erro}
+                            </Alert>
+                        </Collapse>
+
                         <TextField
                             required
                             margin="normal"
                             fullWidth
-                            id="email"
                             label="Email"
-                            name="email"
-                            // autoComplete="email"
                             autoFocus
+                            error={errorEmail == "" ? false : true}
+                            helperText={errorEmail}
+                            onChange={(e) => setEmail(e.currentTarget.value)}
                         />
                         <TextField
                             required
                             margin="normal"
                             fullWidth
-                            name="password"
                             label="Senha"
-                            type="password"
-                            id="password"
-                            // autoComplete="current-password"
+                            error={errorSenha == "" ? false : true}
+                            helperText={errorSenha}
+                            type={showPassword ? "text" : "password"}
+                            onChange={(e) => setPassword(e.currentTarget.value)}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={
+                                                handleMouseDownPassword
+                                            }
+                                            edge="end"
+                                        >
+                                            {showPassword ? (
+                                                <VisibilityOff />
+                                            ) : (
+                                                <Visibility />
+                                            )}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
                         />
-                        {/* <FormControlLabel
-                            control={
-                                <Checkbox value="remember" color="primary" />
-                            }
-                            label="Remember me"
-                        /> */}
-                        <Button
+
+                        <LoadingButton
                             type="submit"
                             fullWidth
                             variant="contained"
                             sx={{ mt: 3, mb: 2 }}
+                            loading={loading}
                         >
                             Continuar
-                        </Button>
+                        </LoadingButton>
+
                         <Grid container style={{ fontSize: "15px" }}>
                             <Grid item xs>
                                 <Link href="/recuperar-senha" variant="body2">
