122068fb3231c233d8abc46afee56bd372c0ab28
Luigi <luigi.ciuffi@hotmail.com>
2022-08-19 23:10:25 -0300

    ListagemCard.js com design certo

--- src/components/ListagemCard.js
+++ src/components/ListagemCard.js
@@ -1,72 +1,80 @@
 import React from "react";
-import { Button, Card, Container, Spinner } from "react-bootstrap";
+import {
+    IconButton,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+} from "@mui/material";
 import * as moment from "moment";
 import NumberFormat from "react-number-format";
-
-const Antigo = ({ i, handleDelete, loading }) => {
-    return (
-        <div className="d-flex align-items-center justify-content-center">
-            <Card className="w-100 mt-3 text-center">
-                <Container
-                    style={
-                        i.tipo == 0
-                            ? { backgroundColor: "#d5f2d5" }
-                            : { backgroundColor: "#f9cccc" }
-                    }
-                    className="d-flex align-items-center justify-content-center p-0"
-                >
-                    <Card.Body style={{ width: "100%" }}>
-                        <strong>Tipo:</strong>
-                        {i.tipo == 0 ? " Entrada" : " Saída"}
-                    </Card.Body>
-                    <Card.Body style={{ width: "100%" }}>
-                        <strong>Valor: </strong>
-                        <NumberFormat
-                            value={i.valor}
-                            thousandSeparator={"."}
-                            decimalSeparator={","}
-                            prefix={"R$"}
-                            decimalScale={2}
-                            fixedDecimalScale={true}
-                            displayType={"text"}
-                        />
-                    </Card.Body>
-                    <Card.Body style={{ width: "100%" }}>
-                        <strong>Data:</strong>
-                        {" " +
-                            moment(i.criado_em.toDate()).format("DD/MM/YYYY")}
-                    </Card.Body>
-                </Container>
-            </Card>
-            <Card style={{ marginInline: "20px" }} className="mt-3">
-                <Button
-                    variant="danger"
-                    onClick={handleDelete}
-                    disabled={loading}
-                >
-                    {loading ? (
-                        <Spinner
-                            as="span"
-                            animation="border"
-                            size="sm"
-                            role="status"
-                            aria-hidden="true"
-                        />
-                    ) : (
-                        "X"
-                    )}
-                </Button>
-            </Card>
-        </div>
-    );
-};
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function ListagemCard(props) {
-    const { i, handleDelete, loading } = props;
+    const { data, handleDelete, loading } = props;
 
     return (
-        <>
-            <Antigo i={i} handleDelete={handleDelete} loading={loading} />
-        </>
+        <div style={{ paddingLeft: "5%", paddingRight: "5%" }}>
+            <Table size="medium">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            <strong>Data</strong>
+                        </TableCell>
+                        <TableCell>
+                            <strong>Tipo</strong>
+                        </TableCell>
+                        <TableCell>
+                            <strong>Valor</strong>
+                        </TableCell>
+                        <TableCell>{""}</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map((item) => (
+                        <TableRow key={item.id}>
+                            <TableCell>
+                                {moment(item.criado_em.toDate()).format(
+                                    "DD/MM/YYYY"
+                                )}
+                            </TableCell>
+                            <TableCell>
+                                {item.tipo == "0" ? "Entrada" : "Saída"}
+                            </TableCell>
+                            <TableCell>
+                                <NumberFormat
+                                    value={item.valor}
+                                    thousandSeparator={"."}
+                                    decimalSeparator={","}
+                                    prefix={"R$"}
+                                    decimalScale={2}
+                                    fixedDecimalScale={true}
+                                    displayType={"text"}
+                                    style={
+                                        item.tipo == 0
+                                            ? { color: "green" }
+                                            : { color: "red" }
+                                    }
+                                />
+                            </TableCell>
+                            <TableCell>
+                                <IconButton
+                                    color="error"
+                                    aria-label="deletar"
+                                    component="label"
+                                    disabled={loading}
+                                    onClick={() => {
+                                        handleDelete(item);
+                                    }}
+                                >
+                                    <DeleteIcon />
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
     );
 }
