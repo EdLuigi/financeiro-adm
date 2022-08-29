aef1140790ea6b4f6868045b5d8e6ca74a5cef5e
Luigi <luigi.ciuffi@hotmail.com>
2022-08-28 22:05:14 -0300

    finalizado redesign adicionar

--- src/pages/Adicionar.js
+++ src/pages/Adicionar.js
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-import {
-    Alert,
-    Button,
-    ButtonGroup,
-    Card,
-    Form,
-    ToggleButton,
-} from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../firebase/authContext";
 import { adicionar } from "../firebase/firestore";
@@ -15,169 +8,141 @@ import Container from "@mui/material/Container";
 import { DrawerCompleto } from "../components/DrawerCompleto";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import {
+    Alert,
     Collapse,
     FormControl,
     FormControlLabel,
     FormLabel,
     Grow,
+    InputAdornment,
     Paper,
     Radio,
     RadioGroup,
+    TextField,
     Toolbar,
 } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
+import { red } from "@mui/material/colors";
+import { green } from "@mui/material/colors";
+import { Box } from "@mui/system";
 
 const mdTheme = createTheme();
 
-const Antigo = () => {
-    const [loading, setLoading] = useState(false);
-    const [erro, setErro] = useState("");
-    const [sucesso, setSucesso] = useState(false);
-    const [tipo, setTipo] = useState(0);
-    const { currentUser } = useAuth();
-    const [valor, setValor] = useState("");
-    const navigate = useNavigate();
-    const MAX_VAL = ({ value }) => value <= 1000000000;
-
-    const handleRadio = (e) => setTipo(e.currentTarget.value);
-
-    const handleInput = (values) => {
-        const { formattedValue, value } = values;
-        // formattedValue = $2,223
-        // value ie, 2223
-        setValor(value);
-    };
-
-    const submit = async (e) => {
-        e.preventDefault();
-
-        if (valor == 0) {
-            setErro("Insira um valor maior que R$0,00");
-            return;
-        }
-
-        try {
-            setSucesso(false);
-            setLoading(true);
-            setErro("");
-
-            await adicionar(currentUser.uid, Number(tipo), Number(valor));
-            setSucesso(true);
-        } catch (e) {
-            console.log("erro: " + e);
-            setErro("Não foi possível adicionar lançamento");
-        }
-        setLoading(false);
-    };
-
+function ComponenteRadioButton({ radioButtonValor, setRadioButtonValor }) {
     return (
-        <>
-            {/* <Card className="mb-4 w-75 p-3 "> */}
-            {/* <Card.Body style={{ paddingInline: "50px" }}> */}
-            <Collapse in={erro != ""}>
-                <Alert variant="danger">{erro}</Alert>
-            </Collapse>
-            <Collapse in={sucesso}>
-                <Alert variant="success">
-                    Lançamento adicionado com sucesso
-                </Alert>
-            </Collapse>
-
-            <div>
-                <Form onSubmit={submit}>
-                    <div className="mb-3">
-                        <label
-                            style={{
-                                marginRight: "10px",
-                            }}
-                        >
-                            Tipo de lançamento:
-                        </label>
-                        <ButtonGroup>
-                            <ToggleButton
-                                id={0}
-                                type="radio"
-                                variant={"outline-primary"}
-                                value={0}
-                                checked={tipo == 0}
-                                onChange={(e) => handleRadio(e)}
-                            >
-                                Entrada
-                            </ToggleButton>
-                            <ToggleButton
-                                id={1}
-                                type="radio"
-                                variant={"outline-primary"}
-                                value={1}
-                                checked={tipo == 1}
-                                onChange={(e) => handleRadio(e)}
-                            >
-                                Saída
-                            </ToggleButton>
-                        </ButtonGroup>
-                    </div>
-
-                    <div>
-                        <Form.Group className="mb-4 ">
-                            <Form.Label>Valor do lançamento:</Form.Label>
-                            <NumberFormat
-                                thousandSeparator={"."}
-                                decimalSeparator={","}
-                                allowNegative={false}
-                                prefix={"R$"}
-                                decimalScale={2}
-                                isAllowed={MAX_VAL}
-                                customInput={Form.Control}
-                                className="w-100"
-                                placeholder="ex.: R$50,00"
-                                required
-                                autoFocus
-                                onValueChange={(values) => {
-                                    handleInput(values);
-                                }}
-                            />
-                            <Form.Text className="text-muted p-2">
-                                insira um valor até R$1.000.000.000,00
-                            </Form.Text>
-                        </Form.Group>
-                        <LoadingButton
-                            type="submit"
-                            disabled={loading}
-                            fullWidth
-                            variant="contained"
-                        >
-                            {loading ? "Carregando..." : "Adicionar"}
-                        </LoadingButton>
-                    </div>
-                </Form>
-            </div>
-            {/* </Card.Body> */}
-            {/* </Card> */}
-        </>
-    );
-};
-
-function RadioTeste() {
-    const [radioButtonValor, setRadioButtonValor] = useState(0);
-    return (
-        <FormControl>
-            <FormLabel>Tipo de lançamento</FormLabel>
+        <FormControl sx={{ mb: 1 }}>
+            <FormLabel focused={false} sx={{ color: "black" }}>
+                Tipo de lançamento:
+            </FormLabel>
             <RadioGroup row defaultValue={0}>
                 <FormControlLabel
-                    control={<Radio />}
+                    control={
+                        <Radio
+                            sx={{
+                                // color: green[700],
+                                "&.Mui-checked": {
+                                    color: green[700],
+                                },
+                            }}
+                        />
+                    }
+                    sx={{
+                        color: radioButtonValor == 0 ? green[700] : "#444444",
+                    }}
                     label="Entrada"
-                    value={radioButtonValor}
+                    value={0}
                     onChange={(e) => {
                         setRadioButtonValor(e.target.value);
                     }}
                 />
-                <FormControlLabel value={1} control={<Radio />} label="Saída" />
+                <FormControlLabel
+                    value={1}
+                    control={
+                        <Radio
+                            sx={{
+                                // color: red[700],
+                                "&.Mui-checked": {
+                                    color: red[700],
+                                },
+                            }}
+                        />
+                    }
+                    sx={{
+                        color: radioButtonValor == 1 ? red[700] : "#444444",
+                    }}
+                    label="Saída"
+                    onChange={(e) => {
+                        setRadioButtonValor(e.target.value);
+                    }}
+                />
             </RadioGroup>
         </FormControl>
     );
 }
 
 export default function Adicionar() {
+    const [loading, setLoading] = useState(false);
+    const [erro, setErro] = useState("");
+    const [sucesso, setSucesso] = useState(false);
+    const { currentUser } = useAuth();
+    const [valor, setValor] = useState("");
+    const [valorEmpty, setValorEmpty] = useState(false);
+    const [radioButtonValor, setRadioButtonValor] = useState(0);
+    const MAX_VAL = ({ value }) => value <= 1000000000;
+
+    const handleInput = (values) => {
+        const { formattedValue, value } = values;
+        // formattedValue = $2,223
+        // value ie, 2223
+        // setValor(values.currentTarget.value);
+        // let value = values.currentTarget.value;
+        // console.log("value: " + value);
+        // setValor(value.replace(",", "."));
+        setValor(value);
+    };
+
+    const validateValor = (valor) => {
+        let valorFinal = valor;
+
+        // if (valorFinal == "") {
+        //     setValorEmpty(true);
+        //     return false;
+        // }
+        if (valorFinal == 0) {
+            setErro("Insira um valor maior que R$0,00.");
+            return false;
+        }
+
+        return true;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setValorEmpty(false);
+            setSucesso(false);
+            setLoading(true);
+            setErro("");
+
+            const valorFinal = Number(
+                valor.replace(/[.]/g, "").replace(/[,]/, ".")
+            );
+
+            if (validateValor(valorFinal)) {
+                await adicionar(
+                    currentUser.uid,
+                    Number(radioButtonValor),
+                    valorFinal
+                );
+                setSucesso(true);
+            }
+        } catch (e) {
+            console.log("erro: " + e);
+            setErro("Não foi possível adicionar o lançamento.");
+        }
+        setLoading(false);
+    };
+
     return (
         <ThemeProvider theme={mdTheme}>
             <DrawerCompleto title="Adicionar Lançamentos">
@@ -188,12 +153,66 @@ export default function Adicionar() {
                     className=" align-items-center justify-content-center"
                 >
                     <Grow in={true}>
-                        <Paper
-                            // sx={{ pt: 6, pb: 5, pl: "7%", pr: "7%" }}
-                            sx={{ p: 6, paddingInline: "15%" }}
-                        >
-                            <Antigo />
-                            {/* <RadioTeste /> */}
+                        <Paper sx={{ p: 6, paddingInline: "15%" }}>
+                            <Collapse in={erro != ""}>
+                                <Alert severity="error">{erro}</Alert>
+                            </Collapse>
+                            <Collapse in={sucesso} sx={{ mb: 2 }}>
+                                <Alert severity="success">
+                                    {"Lançamento adicionado com sucesso."}
+                                </Alert>
+                            </Collapse>
+
+                            <Box
+                                component="form"
+                                onSubmit={handleSubmit}
+                                noValidate
+                            >
+                                <ComponenteRadioButton
+                                    radioButtonValor={radioButtonValor}
+                                    setRadioButtonValor={setRadioButtonValor}
+                                />
+
+                                <NumberFormat
+                                    margin="normal"
+                                    sx={{ w: "75%", mb: 3 }}
+                                    label="Valor do lançamento"
+                                    helperText={
+                                        "insira um valor até R$1.000.000.000,00"
+                                    }
+                                    value={valor}
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                R$
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    thousandSeparator={"."}
+                                    decimalSeparator={","}
+                                    allowNegative={false}
+                                    decimalScale={2}
+                                    isAllowed={MAX_VAL}
+                                    customInput={TextField}
+                                    placeholder="ex.: 50,00"
+                                    required
+                                    autoFocus
+                                    error={valorEmpty}
+                                    onChange={(e) => {
+                                        setValor(e.currentTarget.value);
+                                    }}
+                                />
+
+                                <LoadingButton
+                                    type="submit"
+                                    disabled={loading}
+                                    fullWidth
+                                    variant="contained"
+                                    loading={loading}
+                                >
+                                    {"Adicionar"}
+                                </LoadingButton>
+                            </Box>
                         </Paper>
                     </Grow>
                 </Container>
